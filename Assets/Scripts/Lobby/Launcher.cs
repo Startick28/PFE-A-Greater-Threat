2dbcf640228f4744aa7ad574f3818b6640e916b2
Renox91 <50325665+Renox91@users.noreply.github.com>
2021-10-23 12:52:31 +0200

    Fin du lobby (Back avec UI de base et sans mot de passe)
    Lobby fait avec :
    - Création d'une room
    - Migration automatique d'host
    - Lancement de partie disponible uniquement par l'host de la partie
    - Affichage de tous les joueurs présents dans la room
    - Affichage de toutes les rooms actuellement joignables
    - Changement de scène collectif lorsque l'host lance la partie

--- Assets/Scripts/Lobby/Launcher.cs
+++ Assets/Scripts/Lobby/Launcher.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using Photon.Realtime;
+using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
     [SerializeField] InputField roomNameInputField;
     [SerializeField] Text errorText;
     [SerializeField] Text roomNameText;
-    // Start is called before the first frame update
+
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+
+    [SerializeField] Transform playerListContent;
+    [SerializeField] GameObject playerListItemPrefab;
+
+    [SerializeField] GameObject startGameButton;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -19,11 +34,13 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     public override void OnJoinedLobby()
     {
         MenuManager.Instance.OpenMenu("mainMenu");
+        PhotonNetwork.NickName = "Player" + Random.Range(0, 1000).ToString("0000");
     }
 
     public void CreateRoom()
@@ -40,6 +57,25 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         MenuManager.Instance.OpenMenu("roomMenu");
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach(Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
+
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -54,8 +90,41 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loadingMenu");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loadingMenu");
+        
+    }
+
+    public void StartGame()
+    {
+        // Permet de charger la scene que l'on veut, le 1 représente l'index de la scene indiqué dans le build settings d'unity
+        PhotonNetwork.LoadLevel(1);
+    }
+
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("mainMenu");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            if (roomList[i].RemovedFromList)
+                continue;
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
 }
