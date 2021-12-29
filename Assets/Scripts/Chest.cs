b9f8a75029d970ecf3347732253bb8af400b3348
Renox <renox91250@hotmail.fr>
2021-12-29 16:04:22 +0100

    début coffre anim

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -7,11 +7,15 @@ public class Chest : Interactable
 {
     [SerializeField] RarityType rarity;
     [SerializeField] BasicGun prefabGun;
+    [SerializeField] bool isOpened;
+    [SerializeField] Rigidbody topChestRigidBody;
 
     [PunRPC]
     public override void interact(FPSCharacterController player)
     {
         Debug.Log("interacted with chest");
+        if (!isOpened)
+            OpenAnimation();
 
         if(players.Count == 0)
         {
@@ -43,15 +47,28 @@ public class Chest : Interactable
     public override void finishInteraction()
     {
         canInteract = false;
-        GetComponent<MeshRenderer>().material.color = Color.red * Color.grey;
+        isOpened = true;
     }
 
+    public void OpenAnimation()
+    {
+        topChestRigidBody.angularVelocity = new Vector3(1f, 0, 0);
+        StartCoroutine(stopOpening());
+    }
+
+    IEnumerator stopOpening()
+    { 
+        yield return new WaitForSeconds(4f);
+        Debug.Log("fin anim");
+        topChestRigidBody.angularVelocity = new Vector3(0, 0, 0);
+    }
     // Start is called before the first frame update
     void Start()
     {
         iType = InteractionType.chest;
         collider = GetComponent<BoxCollider>();
         canInteract = true;
+        isOpened = false;
         
     }
 
