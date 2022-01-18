e241a98ee94770ba17cf8b620afab660b265f652
Renox <renox91250@hotmail.fr>
2022-01-18 14:16:27 +0100

    Ajout de l'aléatoire dans les coffres

--- Assets/Scripts/Scripts/Props/Chest.cs
+++ Assets/Scripts/Scripts/Props/Chest.cs
@@ -12,7 +12,7 @@ public class Chest : Interactable
 
     [Header("Chest Gun Setup")]
     [SerializeField] RarityType rarity;
-    [SerializeField] BasicGun prefabGun;
+    [SerializeField] List<GameObject> prefabGun;
     [SerializeField] bool isOpened;
     
 
@@ -32,33 +32,35 @@ public class Chest : Interactable
 
     [PunRPC]
     public override void interact(FPSCharacterController player)
-    {       
+    {
 
-        if(players.Count == 0)
+        if (players.Count == 0)
         {
             return;
             Debug.Log("Error this function should not be able to be called if no players or entity are around");
         }
         //Pour l'instant on augmente les stats du premier joueur à s'être approché du coffre
-        BasicGun playerGun = player.GetComponentInChildren<BasicGun>();
-        if(playerGun == null)
+        GameObject playerGun = player.getCurrentWeapon();
+        if (playerGun == null)
         {
             Debug.Log("Error demanded basic or gun MeshRenderer gun was not found for chest interaction");
         }
         //Remove chest's collider
         gameObject.GetComponent<BoxCollider>().enabled = false;
 
-        BasicGun gun = Instantiate(prefabGun, transform.position ,Quaternion.identity);
+        int randomGunIndex = Random.Range(0, prefabGun.Count);
+        Debug.Log("RandomGunIndex : " + randomGunIndex);
+        GameObject gun = Instantiate(prefabGun[randomGunIndex], transform.position ,Quaternion.identity);
         gun.transform.Translate(0,0,- (gun.transform.localScale.x / 4));
-        gun.Rarity = rarity;
-        gun.canInteract = true;
+        gun.GetComponent<BasicGun>().Rarity = rarity;
+        gun.GetComponent<BasicGun>().canInteract = true;
         gun.GetComponent<BoxCollider>().enabled = true;
         gun.GetComponentInChildren<MeshRenderer>().enabled = true;
         // Default layer
         gun.gameObject.layer = 0;
         gun.GetComponentInChildren<MeshRenderer>().material.color = Color.yellow;
 
-        player.GetComponent<FPSCharacterController>().nearestInteractable = gun;
+        player.GetComponent<FPSCharacterController>().nearestInteractable = gun.GetComponent<BasicGun>();
 
         // Animations
         if (!isOpened)
